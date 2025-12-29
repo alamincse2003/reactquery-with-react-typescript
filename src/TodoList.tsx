e9c4ea837f340467640201b0dd0c19dc951a94c8
alamincse2003 <mdalamincse2003@gmail.com>
2025-12-29 22:24:10 +0600

    InfiniteQuery implement

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,42 +1,48 @@
-import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const TodoList = () => {
-  const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data: posts, isLoading, error } = usePosts({ page, pageSize });
+  const pageSize = 20;
+
+  const {
+    data,
+    error,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = usePosts({ pageSize });
+
   if (isLoading)
     return (
       <p className="text-xl text-center text-green-500">Loading..........</p>
     );
+
   if (error)
     return <p className="text-3xl text-center text-red-500">{error.message}</p>;
+
   return (
     <div>
       <h3 className="text-center text-green-500 text-2xl mt-5 mb-5 underline">
         Posts Data
       </h3>
 
-      {posts?.map((post) => (
-        <div key={post.id} className="  border-2 border-blue-500 p-4 m-4">
-          <h3 className="text-xl">
-            ID: {post.id} . Title: {post.title}
-          </h3>
-        </div>
-      ))}
-      <div className="flex justify-center gap-4 mb-5">
+      {data?.pages.map((page) =>
+        page.map((post) => (
+          <div key={post.id} className="border-2 border-blue-500 p-4 m-4">
+            <h3 className="text-xl">
+              ID: {post.id} . Title: {post.title}
+            </h3>
+          </div>
+        ))
+      )}
+
+      <div className="flex justify-center mb-5">
         <button
-          disabled={page === 1}
-          onClick={() => setPage(page - 1)}
-          className="bg-blue-500 text-white p-2 rounded-md  cursor-pointer"
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+          className="bg-green-500 text-white p-2 rounded-md cursor-pointer disabled:opacity-50"
         >
-          Previous
-        </button>
-        <button
-          onClick={() => setPage(page + 1)}
-          className="bg-green-500 text-white p-2 rounded-md cursor-pointer"
-        >
-          Next
+          {isFetchingNextPage ? "Loading..." : "Load More"}
         </button>
       </div>
     </div>
--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,30 +1,48 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-type Post = {
+export type Post = {
   id: number;
   title: string;
   body: string;
 };
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
-const usePosts = (query: PostQuery) =>
-  useQuery<Post[], Error>({
-    queryKey: ["posts", query],
-    queryFn: () =>
-      axios
-        .get("https://jsonplaceholder.typicode.com/posts", {
-          params: {
-            _start: (query.page - 1) * query.pageSize,
-            _limit: query.pageSize,
-          },
-        })
-        .then((res) => res.data),
-    staleTime: 1 * 60 * 1000,
+const fetchPosts = async ({
+  pageParam = 1,
+  pageSize,
+}: {
+  pageParam: number;
+  pageSize: number;
+}): Promise<Post[]> => {
+  const res = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts",
+    {
+      params: {
+        _start: (pageParam - 1) * pageSize,
+        _limit: pageSize,
+      },
+    }
+  );
+
+  return res.data;
+};
+
+const usePosts = ({ pageSize }: PostQuery) => {
+  return useInfiniteQuery({
+    queryKey: ["posts", pageSize],
+    queryFn: ({ pageParam }) => fetchPosts({ pageParam, pageSize }),
+
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length === pageSize ? allPages.length + 1 : undefined;
+    },
+
+    staleTime: 1 * 60 * 1000, // 1 minute
+    initialPageParam: 1,
   });
+};
 
 export default usePosts;
